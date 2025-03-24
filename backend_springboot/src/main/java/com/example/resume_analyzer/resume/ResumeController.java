fda184d2794b36e86435b88984bec3c3c8308743
Lunado <liendhhha140217@gmail.com>
2025-03-24 23:08:01 +0900

    Extract text from pdf file

---
+++ backend_springboot/src/main/java/com/example/resume_analyzer/resume/ResumeController.java
@@ -0,0 +1,18 @@
+package com.example.resume_analyzer.resume;
+
+import com.example.resume_analyzer.resume.dto.ResumeRequest;
+import com.example.resume_analyzer.resume.service.ResumeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("resume")
+public class ResumeController {
+    private final ResumeService resumeService;
+    @PostMapping
+    public String extractFile(@RequestParam MultipartFile file){
+        return resumeService.extractText(file);
+    }
+}
