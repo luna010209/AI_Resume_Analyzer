67a2972818f8f8ef0fa13a5e6301dc0243351065
Lunado <liendhhha140217@gmail.com>
2025-03-30 12:57:11 +0900

    Training model

--- ai_nlp/nlp_model.py
+++ ai_nlp/nlp_model.py
@@ -1,6 +1,9 @@
 import spacy
 import re
+import nltk
 from transformers import pipeline
+
+nltk.download('stopwords')
 
 # Load spaCy model
 nlp= spacy.load("en_core_web_sm")
@@ -30,3 +33,39 @@ def extract_skills(text):
   return {"Skills": found_skills}
 
 
+import pandas as pd
+import base64, random
+import time, datetime
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io, random
+from streamlit_tags import st_tags
+from PIL import Image
+import pymysql
+import pafy
+import plotly.express as px
+
+def fetch_yt_video(link):
+  video = pafy.new(link)
+  return video.title
+
+def get_table_download_link(df, filename, text):
+  """Generates a link allowing the data in a given panda dataframe to be downloaded
+  in: dataframe
+  out: href string
+  """
+  csv = df.to_csv(index= False)
+  b64 = base64.b64encode(csv.encode()).decode()
+  href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+  return href
+
+def pdf_reader(file):
+  resource_manager = PDFResourceManager()
+  fake_file_handle = io.StringIO()
+  converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+  page_interpreter = PDFPageInterpreter(resource_manager, converter)
+  
